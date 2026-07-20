8fa3c323601b78f9ed69cb3c63eb361a8f3a5f7e
ziczac309 <bhavydeshmuk@gmail.com>
2026-07-20 20:50:45 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,47 +1,86 @@
-// Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import {
-  getAuth,
-  GoogleAuthProvider,
-  signInWithPopup,
-  signOut,
-  onAuthStateChanged
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
-import {
-  getDatabase
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
-
-// 🔥 Apni Firebase Config yahan paste karna
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "XXXXXXXX",
-  appId: "XXXXXXXX"
+  apiKey: "AIzaSyBZnHJQ5sR2nrME9U4_dyum80lOBSqybqY",
+  authDomain: "gameresports-b36ac.firebaseapp.com",
+  databaseURL: "https://gameresports-b36ac-default-rtdb.firebaseio.com",
+  projectId: "gameresports-b36ac",
+  storageBucket: "gameresports-b36ac.firebasestorage.app",
+  messagingSenderId: "478733206685",
+  appId: "1:478733206685:web:476ee4cb69f3c717154074"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getDatabase(app);
+export const storage = getStorage(app);
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import {
+  ref,
+  set
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
 const provider = new GoogleAuthProvider();
 
-export function login() {
-  return signInWithPopup(auth, provider);
-}
+window.addEventListener("DOMContentLoaded", () => {
 
-export function logout() {
-  return signOut(auth);
-}
+    const loginBtn = document.getElementById("googleLogin");
 
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    console.log("Welcome:", user.displayName);
-  } else {
-    console.log("User not logged in");
-  }
+    if(loginBtn){
+
+        loginBtn.onclick = async () => {
+
+            try{
+
+                const result = await signInWithPopup(auth, provider);
+
+                const user = result.user;
+
+                await set(ref(db,"users/"+user.uid),{
+
+                    uid:user.uid,
+                    name:user.displayName,
+                    email:user.email,
+                    photo:user.photoURL,
+                    wallet:0,
+                    role:"user"
+
+                });
+
+                alert("Login Successful");
+
+                location.href="index.html";
+
+            }catch(err){
+
+                alert(err.message);
+
+            }
+
+        }
+
+    }
+
+});
+onAuthStateChanged(auth,(user)=>{
+
+    if(user){
+
+        console.log("Logged In :",user.displayName);
+
+    }else{
+
+        console.log("Not Logged In");
+
+    }
+
 });
